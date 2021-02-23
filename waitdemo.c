8e033a2b629190ba486bc2463131a5c090ae18f9
krishnacds <79127687+krishnacds@users.noreply.github.com>
2021-02-23 16:35:50 +0530

    Create waitdemo.c

---
+++ waitdemo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+   int pid;
+   int status;
+   pid = fork();   
+   // Child process
+   if (pid == 0) {
+      system("ls -l");
+      sleep(10);
+      system("ls -l");
+      return 3; //exit status is 3 from child process
+   } else {
+      sleep(3);
+      wait(&status);
+      printf("In parent process: exit status from child is decimal %d, hexa %0x\n", status, status);
+   }
+   return 0;
+}
