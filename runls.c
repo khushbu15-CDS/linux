7f4fe18d932693de2859c3ef2e828be8f13994fb
krishnacds <79127687+krishnacds@users.noreply.github.com>
2021-02-23 15:53:13 +0530

    Create runls.c

---
+++ runls.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+   int pid;
+   pid = fork();
+   
+   // Child process
+   if (pid == 0) {
+      system("ls");
+      sleep(10);     
+   } else {
+      sleep(3);
+   }
+   return 0;
+}
